856e09860bbced58026d358c43aaef92cf585174
william <willshinji@gmail.com>
2018-12-05 00:28:47 -0200

    import Hello component

--- ex/index.jsx
+++ ex/index.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-ReactDOM.render(<h1>Hello React</h1>, document.getElementById('app'));
+import Hello from './Hello'
+
+ReactDOM.render(<Hello />, document.getElementById('app'));
