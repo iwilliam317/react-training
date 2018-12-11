932ab9a35ff471803fa99b20c66149f174df565c
william <willshinji@gmail.com>
2018-12-11 12:59:51 -0200

    rename component Counter2 to AutoConter

--- ex/index.jsx
+++ ex/index.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-import Counter2 from './counter2'
+import AutoCounter from './autoCounter'
 
 ReactDOM.render(
-    <Counter2 value={10} />
+    <AutoCounter value={10} />
     , document.getElementById('app'));
