fbed583ca9e48eda7d32af944681bc077b56fc3c
william <willshinji@gmail.com>
2018-12-14 13:25:32 -0200

    receives multiple params

--- ex/form.jsx
+++ ex/form.jsx
@@ -3,25 +3,26 @@ import React, { Component } from 'react'
 class Form extends Component {
     constructor(props){
         super(props)
-        this.state = { name: '' }       
+        this.state = { }       
     }
 
     handleSubmit(event){
-        alert(`A name was submitted: ${this.state.name}`);
+        alert(`Information submitted: ${this.state.name} ${this.state.age}`);
         event.preventDefault();
     }
     handleChange(event){
-        this.setState({ name: event.target.value })
+        this.setState({ [event.target.name]: event.target.value })
     }
     render(){
         return (
             <div>
                 <form onSubmit={ this.handleSubmit.bind(this) }>
                     <label>Name: </label>
-                    <input onChange={ this.handleChange.bind(this) }/>
+                    <input name='name' onChange={ this.handleChange.bind(this) }/>
+                    <label>Age</label>
+                    <input type='number' name='age' onChange={ this.handleChange.bind(this) } />
                     <button type='submit'>Send</button>
-                </form>
-                { this.state.name}       
+                </form>      
             </div>
         )
 
