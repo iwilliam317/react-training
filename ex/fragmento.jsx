9b75a9b15e09d8af911c093d2c28afa2746ef537
william <willshinji@gmail.com>
2019-01-28 15:42:25 -0200

    react.fragment

---
+++ ex/fragmento.jsx
@@ -0,0 +1,14 @@
+import React,  { Component, Fragment } from 'react'
+
+class Fragmento extends Component {
+    render(){
+        return (
+            <Fragment>
+                <h1>Fragmento 1</h1>
+                <h1>Fragmento 2</h1>
+            </Fragment>
+        )
+    }
+}
+
+export default Fragmento
--- ex/index.jsx
+++ ex/index.jsx
@@ -1,19 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-import { Provider } from 'react-redux'
-import {  createStore } from 'redux'
+import Fragmento from './fragmento'
 
-const devTools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-
-import Todo from './todo'
-
-import reducers from './reducers'
-
-const store = createStore(reducers, devTools)
-
-ReactDOM.render(
-    <Provider store={store}>
-        <Todo />
-    </Provider>, document.getElementById('app')
-)
+ReactDOM.render(<Fragmento />, document.getElementById('app') )
